50b03ffcd6ef1d675e27846cb71b2ff6f1a4199d
fahime <fahimeh757@gmail.com>
2022-09-27 14:58:01 +0330

    style navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,18 +2,17 @@ import "./Navigation.css";
 import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
-    <section className="navigation">
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">HomePage</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cart">Cart</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </section>
+    <nav className="navigation">
+      <h4>Shopping Shoes</h4>
+      <ul>
+        <li>
+          <NavLink to="/">HomePage</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cart">Cart</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
