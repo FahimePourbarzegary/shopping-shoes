c6436a4a7d00ab84a74955869efddb46f6388c78
fahime <fahimeh757@gmail.com>
2022-09-27 16:10:37 +0330

    add product style

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,36 @@
 import Layout from "../Layout/Layout";
-
+import * as data from "../data";
 const HomePage = () => {
-    return ( <Layout>
-        <h2>this is  HomePage</h2>
-    </Layout> );
-}
- 
+  const addProductHandler = (product) => {
+    console.log(product);
+  };
+  return (
+    <Layout>
+      <main className="container">
+        <section className="products">
+          {data.products.map((product) => {
+            return (
+              <section className="product">
+                <div className="product__img">
+                  <img src={product.image} alt="img-shoes" />
+                </div>
+                <div className="product__description">
+                  <p>{product.name}</p>
+                  <p>${product.price}</p>
+                  <button
+                    onClick={() => addProductHandler(product)}
+                    className="btn btn__primary"
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </section>
+            );
+          })}
+        </section>
+      </main>
+    </Layout>
+  );
+};
+
 export default HomePage;
